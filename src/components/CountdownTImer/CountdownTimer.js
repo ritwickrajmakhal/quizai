7cf3fdf1bace110d66a772cc53f87c0fba9a28fc
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-02-26 20:04:35 +0530

    Add CountdownTimer component and related files, PreLoader component style changes, encryptDecrypt and prompt functions

---
+++ src/components/CountdownTImer/CountdownTimer.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import digitalclock from "./digitalclock.png";
+
+const CountdownTimer = ({ time, countdownCompleted, onCountdownComplete }) => {
+  const [seconds, setSeconds] = useState(time * 60);
+
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      if (seconds > 0 && !countdownCompleted) {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      } else {
+        clearInterval(countdownInterval);
+        if (onCountdownComplete) {
+          onCountdownComplete(); // Invoke the callback function
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(countdownInterval);
+  }, [seconds, onCountdownComplete]);
+
+  const formatTime = () => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${hours < 10 ? "0" : ""}${hours}:${
+      minutes < 10 ? "0" : ""
+    }${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
+  return (
+    <div className="position-relative w-auto">
+      <img className="image-fluid" src={digitalclock} alt="Digital Clock" />
+      <p className="text-center fs-2 position-absolute top-50 start-50 translate-middle">
+        {formatTime()}
+      </p>
+    </div>
+  );
+};
+
+export default CountdownTimer;
--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -1,11 +1,13 @@
 import React from "react";
 import loading from "./loading.gif";
-import "./preLoaderStyle.css";
 
-export default function PreLoader() {
+export default function PreLoader({ msg }) {
   return (
-    <div className="preloader">
-      <img alt="loader" className="image-responsive" src={loading} />
+    <div className="preloader position-absolute z-3 vw-100 vh-100" style={{ backgroundColor: 'rgba(0,0,0,0.3)' }}>
+      <div className="d-flex justify-content-center align-items-center flex-column position-relative top-50">
+        <img alt="loader" className="image-responsive" src={loading} style={{ width: '100px' }} />
+        <h3 className="text-white fw-bold">{msg}</h3>
+      </div>
     </div>
   );
 }
