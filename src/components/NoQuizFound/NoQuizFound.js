0285fe380e42db61e6d3600087b37b535bc00df2
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-03-08 11:29:03 +0530

    Quiz not found component added

---
+++ src/components/NoQuizFound/NoQuizFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function NoQuizFound() {
+  return (
+    <div className="mt-5 notFound">
+      <h1 className="neonText">404</h1>
+      <h2 className="neonText">NO QUIZ FOUND</h2>
+    </div>
+  );
+}
