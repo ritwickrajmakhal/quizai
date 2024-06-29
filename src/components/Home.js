eb61051333dec8d3ac4aee2262a82a69fb4ec084
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-06-29 06:49:00 +0530

    chore: Update Home component to use config file for cover image and website name

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,52 +1,43 @@
-import React, { useEffect, useState } from "react";
 import TypingAnimation from "./TypingAnimation";
 import PropTypes from "prop-types";
 import { useGoogleLogin } from "@react-oauth/google";
-import request from "../func/request";
+import config from "../config.json";
+import coverImage from "./coverImage.jpg";
 
 export default function Home(props) {
   // set title of the web page
   document.title = "Quiz AI - Login";
-  // Getting data from website's backend api
-  const [webSiteData, setWebSiteData] = useState();
-  useEffect(() => {
-    request("/api/website?populate=*", "GET").then((res) => {
-      setWebSiteData(res?.data.attributes);
-    });
-  }, []);
 
   const login = useGoogleLogin({
     onSuccess: (codeResponse) => {
       props.setUser(codeResponse);
       localStorage.setItem("user", JSON.stringify(codeResponse));
     },
-    onError: (error) => {},
+    onError: (error) => { },
   });
 
   return (
     <div className="d-flex justify-content-center my-5">
-      {webSiteData && (
-        <div className="card text-bg-dark shadow-lg" style={{ width: "35rem" }}>
-          <img
-            src={webSiteData.coverImage.data.attributes.url}
-            className="card-img img-fluid"
-            alt="Cover"
-          />
+      <div className="card text-bg-dark shadow-lg" style={{ width: "35rem" }}>
+        <img
+          src={coverImage}
+          className="card-img img-fluid"
+          alt="Cover"
+        />
 
-          <div
-            className="card-img-overlay d-flex align-items-start flex-column"
-            style={{ backgroundColor: "rgba(0,0,0,0.3)" }}
-          >
-            <h1 className="card-title">{webSiteData.name}</h1>
-            <p className="card-text fs-4">
-              <TypingAnimation texts={webSiteData.quotes} speed={100} />|
-            </p>
-            <button className="btn btn-lg btn-dark mt-auto" onClick={login}>
-              <i className="fa-brands fa-google"></i> Login with Google
-            </button>
-          </div>
+        <div
+          className="card-img-overlay d-flex align-items-start flex-column"
+          style={{ backgroundColor: "rgba(0,0,0,0.3)" }}
+        >
+          <h1 className="card-title">{config.name}</h1>
+          <p className="card-text fs-4">
+            <TypingAnimation texts={config.quotes} speed={100} />|
+          </p>
+          <button className="btn btn-lg btn-dark mt-auto" onClick={login}>
+            <i className="fa-brands fa-google"></i> Login with Google
+          </button>
         </div>
-      )}
+      </div>
     </div>
   );
 }
